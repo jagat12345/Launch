3c33b43f266b798b76da57ef73ae2e6b342a9d0b
Priyanka <Priyanka@DESKTOP-5EL6MDO>
2019-07-12 10:51:06 +0530

    commit two

--- HybridDrivenFramework/src/com/abc/magento/Magento.java
+++ HybridDrivenFramework/src/com/abc/magento/Magento.java
@@ -1,14 +1,13 @@
 package com.abc.magento;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Magento {
 
@@ -26,7 +25,7 @@ public class Magento {
 		String nlogin    = p.getProperty("login");
 	    String nlogout   = p.getProperty("logout");
 	    
-	    WebDriver driver = new ChromeDriver();
+	    WebDriver driver = new FirefoxDriver();
 	    driver.manage().window().maximize();
 	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	    driver.get(nurl);
